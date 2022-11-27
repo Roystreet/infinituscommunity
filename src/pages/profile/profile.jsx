2f1e9a90bce5e168c886528d0f808db956307233
Derek Cabrera <cabrera.derek.adriel@gmail.com>
2022-11-27 13:21:36 -0300

    Maquetando info profile

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -1,3 +1,37 @@
 export default function Profile() {
-  return <p> Profle </p>;
+  return(
+    <>
+
+      <div>
+        <h1>Welcome!</h1>
+      </div>
+      <div>
+        <div>
+          <p>imagen</p>
+          <h2>Nombre</h2>
+        </div>
+        <div>
+          <h3>Cuenta</h3>
+          <div>
+            <span>numero hasheados</span>
+            <p>cop</p>
+          </div>
+        </div>
+        <div>
+          <h3>Balances</h3>
+          <div>
+            <h4>Busd Token</h4>
+            <span>6511</span>
+          </div>
+          <div>
+            <h4>Infinitus Token</h4>
+            <span>6511</span>
+            <span>Icon</span>
+          </div>
+        </div>
+        
+      </div>
+    </>
+  );
 }
+    
