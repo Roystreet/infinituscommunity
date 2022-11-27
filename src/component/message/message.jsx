02366f8f3ebcaf011d9797b9a93dad377d011594
Roiner Bracamonte <rbracamonte.winhold@gmail.com>
2022-11-27 03:38:29 -0500

    agregado el pantallaso cuando exceda el tamano de la pantalla

--- src/component/message/message.jsx
+++ src/component/message/message.jsx
@@ -3,7 +3,12 @@ import { Box } from "@mui/material";
 export default function Message() {
   return (
     <Box className="hidden-message" component="div">
-      <p> Hola aqui estoy </p>
+      <Box component="div" className="container-message center">
+        <Box component="p">
+          Hola! Estamos felices de verte aqui. Para tener una mejor experiencia,
+          abre la app en tu celular ;D
+        </Box>
+      </Box>
     </Box>
   );
 }
