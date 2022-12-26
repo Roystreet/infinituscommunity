a352ae8e5486ab9e31da8a73f1c563bab122483f
Ivan H <ivan2herrerra@gmail.com>
2022-12-26 11:04:48 -0400

    cardshare adjustments

--- src/component/cardShare/cardShare.jsx
+++ src/component/cardShare/cardShare.jsx
@@ -1,101 +1,90 @@
 import { useState } from "react";
 import style from "./cardShare.module.css";
 import img from "./assets/Vector.png";
-import { FaUser, FaRegUser,} from "react-icons/fa";
+import { FaUser, FaRegUser } from "react-icons/fa";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import "bootstrap/dist/css/bootstrap.min.css";
 import CloseButton from "react-bootstrap/CloseButton";
 
-const CardShare = ({img, referals, id}) => {
-  function mosImg(imgRoute){
-    if (imgRoute) {
-      return `../../../public/packagesAvatar/${imgRoute}.png`
-    } else {
-      return "../../../public/packagesAvatar/iniciado.png"
-    }
-  }
-  const [smShow, setSmShow] = useState(false);
-  function iconRegUser(referals) {
-    const expr = referals;
-    switch (expr) {
-      case 0:
-        
-        return(
-          <div>
-              <FaRegUser className={style.iconUser}/>
-              <FaRegUser className={style.iconUser}/>
-             <FaRegUser className={style.iconUser}/>
-            <FaRegUser className={style.iconUser}/>
-          </div>
-      
-        )
-       
-      case 1:
-        return(
-          <div>
-              <FaUser className={style.iconUser}/>
-              <FaRegUser className={style.iconUser}/>
-             <FaRegUser className={style.iconUser}/>
-            <FaRegUser className={style.iconUser}/>
-          </div>
-      
-        )
-     case 2:
-      return(
-        <div>
-            <FaUser className={style.iconUser}/>
-            <FaUser className={style.iconUser}/>
-           <FaRegUser className={style.iconUser}/>
-          <FaRegUser className={style.iconUser}/>
-        </div>
-    
-      )
-      case 3:
-        return(
-          <div>
-              <FaUser className={style.iconUser}/>
-              <FaUser className={style.iconUser}/>
-             <FaUser className={style.iconUser}/>
-            <FaRegUser className={style.iconUser}/>
-          </div>
-      
-        )
-      case 4:
-        return(
-          <div>
-              <FaUser className={style.iconUser}/>
-              <FaUser className={style.iconUser}/>
-             <FaUser className={style.iconUser}/>
-            <FaUser className={style.iconUser}/>
-          </div>
-      
-        )
-      default:
-        console.log(`Sorry, we are out of ${expr}.`);
-    }
-    
-  }
-  
+const CardShare = ({ img, referals, id }) => {
+	function mosImg(imgRoute) {
+		if (imgRoute) {
+			return `../../../public/packagesAvatar/${imgRoute}.png`;
+		} else {
+			return "../../../public/packagesAvatar/iniciado.png";
+		}
+	}
+	const [smShow, setSmShow] = useState(false);
+	function iconRegUser(referals) {
+		const expr = referals;
+		switch (expr) {
+			case 0:
+				return (
+					<div>
+						<FaRegUser className={style.iconUser} />
+						<FaRegUser className={style.iconUser} />
+						<FaRegUser className={style.iconUser} />
+						<FaRegUser className={style.iconUser} />
+					</div>
+				);
 
-  return (
-    <div className={style.card}>
-      <div>
-        <div>
-        <img className={style.img} src={mosImg(imgRoute)} alt="" />
-          <div className={style.contIcon}>
-            <span className={style.icons}>
-            {iconRegUser(referals)}
-            </span>
-        
-          </div>
-        </div>
-      </div>
+			case 1:
+				return (
+					<div>
+						<FaUser className={style.iconUser} />
+						<FaRegUser className={style.iconUser} />
+						<FaRegUser className={style.iconUser} />
+						<FaRegUser className={style.iconUser} />
+					</div>
+				);
+			case 2:
+				return (
+					<div>
+						<FaUser className={style.iconUser} />
+						<FaUser className={style.iconUser} />
+						<FaRegUser className={style.iconUser} />
+						<FaRegUser className={style.iconUser} />
+					</div>
+				);
+			case 3:
+				return (
+					<div>
+						<FaUser className={style.iconUser} />
+						<FaUser className={style.iconUser} />
+						<FaUser className={style.iconUser} />
+						<FaRegUser className={style.iconUser} />
+					</div>
+				);
+			case 4:
+				return (
+					<div>
+						<FaUser className={style.iconUser} />
+						<FaUser className={style.iconUser} />
+						<FaUser className={style.iconUser} />
+						<FaUser className={style.iconUser} />
+					</div>
+				);
+			default:
+				console.log(`Sorry, we are out of ${expr}.`);
+		}
+	}
 
-      <div className={style.contBtn}>
-        <button className={style.btn}>Collab</button>
-      </div>
-    </div>
-  );
+	return (
+		<div className={style.card}>
+			<div>
+				<div>
+					<img className={style.img} src={mosImg(img)} alt="" />
+					<div className={style.contIcon}>
+						<span className={style.icons}>{iconRegUser(referals)}</span>
+					</div>
+				</div>
+			</div>
+
+			<div className={style.contBtn}>
+				<button className={style.btn}>Collab</button>
+			</div>
+		</div>
+	);
 };
 export default CardShare;
--- src/pages/invitado/invitado.jsx
+++ src/pages/invitado/invitado.jsx
@@ -1,82 +1,64 @@
- 
- import img from "./assets/Union.png"
- import imgI from "./assets/INFINITUS.png"
- import CardShare from "../../component/CardShare/CardShare";
- import style from "./Invitado.module.css"
- import { useState, useEffect } from 'react';
-import { prepareServerConnection } from "../../functions/serverInteractions";
+import img from "./assets/Union.png";
+import imgI from "./assets/INFINITUS.png";
+import CardShare from "../../component/CardShare/CardShare";
+import style from "./Invitado.module.css";
+import { useState, useEffect } from "react";
+import { sendServerPost } from "../../functions/serverInteractions";
+import { useParams } from "react-router-dom";
 
- 
- 
- export default function Invitado() {
-   // Mostrar Tikets    
-   const [myInfo, setmyInfo] = useState({});
-   const [ticket, setTicket] = useState();
-  
+export default function Invitado() {
+	const { address } = useParams();
+	let { idticket } = useParams();
+	// Mostrar Tikets
+	const [myInfo, setmyInfo] = useState({});
+	const [ticket, setTicket] = useState([]);
 
+	const dataTicket = async () => {
+		idticket = parseInt(idticket);
+		console.log(idticket, address);
+		const ticket = await sendServerPost({ _id: idticket, ownerAddress: address }, "/user/getticketrefered", "json");
+		setTicket(ticket);
+		console.log(ticket);
+	};
 
-  useEffect( () => {
-  const data = async ()=>{
-  setmyInfo(
-    await prepareServerConnection(
-      { address: localStorage.getItem('address') },
-      '/user/getmyinfo',
-      'json',
-      localStorage.getItem('jwt')
-    )
-  ); 
-  }
-  data()
+	useEffect(() => {
+		dataTicket();
 	}, []);
 
-  useEffect( () => {
-    const dataTicket = async ()=>{
-      setTicket(
-        await prepareServerConnection(
-          { _id: 1, 
-            ownerAddress: localStorage.getItem('address') },
-          '/user/getticketrefered',
-           'json',
-        )
-      )
-      }
-      dataTicket()
-    
-  },[])
+	/* 	console.log(ticket);
 
-console.log(ticket)
+	const datos = [];
+	datos.push(ticket);
+	console.log(datos); */
 
-const datos = []
-datos.push(ticket)
-console.log(datos)
-
-   return (
-     <div className={style.content}>
-    <div className={style.contTitle}>
-      <h1 className={style.name}>{myInfo ? (myInfo.nickName) : (X)}</h1>
-      <span className={style.subtitle}>Invites you <br/>to collaborate</span>
-    </div>
-    { 
-      datos ? datos.map((e) => {
-           
-        return(
-        <div className={style.contCard} key={e.ticketId}>
-        <CardShare 
-         img={e.imgRoute}
-         referals = {e.referrals}
-         id = {e.ticketId}
-        />
-       </div>
-       )
-       }): (    <p> You have no tickets </p>)
-         
-     }
-      <div className={style.contLogo}>
-        <span className={style.subtitle}>Colaborate and <br/>Keep it rolling!</span>
-        <img src={img} className={style.img} alt="Logoicon" />
-        <img src={imgI} className={style.imgInf} alt="Logoicon" />
-      </div>
-     </div>
-   );
- }
- 
+	return (
+		<div className={style.content}>
+			<div className={style.contTitle}>
+				<h1 className={style.name}>{myInfo ? myInfo.nickName : X}</h1>
+				<span className={style.subtitle}>
+					Invites you <br />
+					to collaborate
+				</span>
+			</div>
+			{ticket ? (
+				ticket.map((e) => {
+					return (
+						<div className={style.contCard} key={e.ticketId}>
+							<CardShare img={e.imgRoute} referals={e.referrals} id={e.ticketId} />
+						</div>
+					);
+				})
+			) : (
+				<p> You have no tickets </p>
+			)}
+			<div className={style.contLogo}>
+				<span className={style.subtitle}>
+					Colaborate and <br />
+					Keep it rolling!
+				</span>
+				<img src={img} className={style.img} alt="Logoicon" />
+				<img src={imgI} className={style.imgInf} alt="Logoicon" />
+			</div>
+		</div>
+	);
+}
