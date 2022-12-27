999b0cae965509e7d3192b5a8423555f0f9d174f
Roiner Bracamonte <rbracamonte.winhold@gmail.com>
2022-12-27 01:24:55 -0400

    correccion de imagenes

--- src/App.jsx
+++ src/App.jsx
@@ -22,84 +22,115 @@ import DisplayMessage from "./component/displayMessage/displayMessage";
 import Message from "./component/message/message";
 
 function App() {
-	const [userLogged, setUserLogged] = useState(false);
-	const [open, setOpen] = useState(false);
+  const [userLogged, setUserLogged] = useState(false);
+  const [open, setOpen] = useState(false);
 
-	const chekUserLoged = () => {
-		if (localStorage.getItem("jwt") != undefined) {
-			setUserLogged(true);
-		}
-	};
+  const chekUserLoged = () => {
+    if (localStorage.getItem("jwt") != undefined) {
+      setUserLogged(true);
+    }
+  };
 
-	useEffect(() => {
-		chekUserLoged();
-	}, [userLogged]);
+  useEffect(() => {
+    chekUserLoged();
+  }, [userLogged]);
 
-	return (
-		<>
-			{userLogged ? (
-				<Layout>
-					<Routes>
-						<Route path="/" element={<Profile setUserLogged={setUserLogged} />} />
-						<Route
-							path="/login"
-							element={
-								!userLogged ? (
-									<Login setUserLogged={setUserLogged} />
-								) : (
-									<DisplayMessage
-										open={true}
-										setOpen={setOpen}
-										messageData={{ tittle: "Notificacion", message: "Ya estas conectado :)" }}
-										allowBackdropClick={false}
-										exitRoute={"/"}
-									/>
-								)
-							}
-						/>
-						<Route path="/preventa" element={<Presale />} />
-						<Route path="/perfil" element={<Profile setUserLogged={setUserLogged} />} />
-						<Route path="/paquetes" element={<Package />} />
-						<Route path="/error" element={<Error />} />
-						<Route path="/settings" element={<Settings />} />
-						<Route path="/share/:idticket/owner/:address" element={<Invitado userLogged={userLogged} setUserLogged={setUserLogged} />} />
-						<Route
-							path="*"
-							element={
-								<DisplayMessage
-									open={true}
-									setOpen={setOpen}
-									messageData={{ tittle: "Error", message: "Esta ruta no existe o esta en mantenimiento" }}
-									allowBackdropClick={true}
-									exitRoute={"/"}
-								/>
-							}
-						/>
-					</Routes>
-				</Layout>
-			) : (
-				<>
-					<Message />
-					<Routes>
-						<Route path="/" element={<Login setUserLogged={setUserLogged} />} />
-						<Route
-							path="*"
-							element={
-								<DisplayMessage
-									open={!open}
-									setOpen={setOpen}
-									messageData={{ tittle: "Error", message: "Debe Iniciar Sesion " }}
-									allowBackdropClick={true}
-									exitRoute={"/"}
-								/>
-							}
-						/>
-						<Route path="/share/:idticket/owner/:address" element={<Invitado userLogged={userLogged} setUserLogged={setUserLogged} />} />
-					</Routes>
-				</>
-			)}
-		</>
-	);
+  return (
+    <>
+      {userLogged ? (
+        <Layout>
+          <Routes>
+            <Route
+              path="/"
+              element={<Profile setUserLogged={setUserLogged} />}
+            />
+            <Route
+              path="/login"
+              element={
+                !userLogged ? (
+                  <Login setUserLogged={setUserLogged} />
+                ) : (
+                  <DisplayMessage
+                    open={true}
+                    setOpen={setOpen}
+                    messageData={{
+                      tittle: "Notificacion",
+                      message: "Ya estas conectado :)",
+                    }}
+                    allowBackdropClick={false}
+                    exitRoute={"/"}
+                  />
+                )
+              }
+            />
+            <Route path="/preventa" element={<Presale />} />
+            <Route
+              path="/perfil"
+              element={<Profile setUserLogged={setUserLogged} />}
+            />
+            <Route path="/paquetes" element={<Package />} />
+            <Route path="/error" element={<Error />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route
+              path="/share/:idticket/owner/:address"
+              element={
+                <Invitado
+                  userLogged={userLogged}
+                  setUserLogged={setUserLogged}
+                />
+              }
+            />
+            <Route
+              path="*"
+              element={
+                <DisplayMessage
+                  open={true}
+                  setOpen={setOpen}
+                  messageData={{
+                    tittle: "Error",
+                    message: "Esta ruta no existe o esta en mantenimiento",
+                  }}
+                  allowBackdropClick={true}
+                  exitRoute={"/"}
+                />
+              }
+            />
+          </Routes>
+        </Layout>
+      ) : (
+        <>
+          <Message />
+          <Routes>
+            <Route path="/" element={<Login setUserLogged={setUserLogged} />} />
+            <Route
+              path="*"
+              element={
+                <DisplayMessage
+                  open={!open}
+                  setOpen={setOpen}
+                  messageData={{
+                    tittle: "Error",
+                    message: "Debe Iniciar Sesion ",
+                  }}
+                  allowBackdropClick={true}
+                  exitRoute={"/"}
+                />
+              }
+            />
+            <Route
+              path="/share/:idticket/owner/:address"
+              element={
+                <Invitado
+                  userLogged={userLogged}
+                  setUserLogged={setUserLogged}
+                />
+              }
+            />
+          </Routes>
+        </>
+      )}
+    </>
+  );
 }
 
 export default App;
--- src/functions/serverInteractions.js
+++ src/functions/serverInteractions.js
@@ -1,5 +1,7 @@
-const urlServer = import.meta.env.VITE_APP_SERVER_URL;
-const urlOrigin = import.meta.env.VITE_APP_ORIGIN_URL;
+//const urlServer = import.meta.env.VITE_APP_SERVER_URL;
+//const urlOrigin = import.meta.env.VITE_APP_ORIGIN_URL;
+const urlServer = "https://api.infinituscommunity.com";
+const urlOrigin = "https://infinituscommunity.com";
 
 /**Descripcion: Interactua con las rutas Post. -
  * Entradas:
@@ -9,17 +11,27 @@ const urlOrigin = import.meta.env.VITE_APP_ORIGIN_URL;
  * jwt: JSON Web Token, en caso de ser una llamada de autenticacion omitir este parametro -
  * Retorna: Respuesta del servidor
  */
-export const sendServerPost = async (params, route, output, jwt = undefined) => {
-	const response = await ServerConnection(route, "POST", JSON.stringify(params), jwt);
-	if (response.status == 200) {
-		const responseHanded = await ServerResponseHandler(response, output);
-		return responseHanded;
-	} else if (response.status == undefined) {
-		return response;
-	} else {
-		const responseHanded = await ServerResponseHandler(response, output);
-		return responseHanded;
-	}
+export const sendServerPost = async (
+  params,
+  route,
+  output,
+  jwt = undefined
+) => {
+  const response = await ServerConnection(
+    route,
+    "POST",
+    JSON.stringify(params),
+    jwt
+  );
+  if (response.status == 200) {
+    const responseHanded = await ServerResponseHandler(response, output);
+    return responseHanded;
+  } else if (response.status == undefined) {
+    return response;
+  } else {
+    const responseHanded = await ServerResponseHandler(response, output);
+    return responseHanded;
+  }
 };
 
 /**Descripcion: Interactua con las rutas Get -
@@ -29,16 +41,16 @@ export const sendServerPost = async (params, route, output, jwt = undefined) => 
  *Retorna: Respuesta del servidor
  */
 export const sendServerGet = async (route, output) => {
-	const response = await ServerConnection(route, "GET", undefined, undefined);
-	if (response.status == 200) {
-		const responseHanded = await ServerResponseHandler(response, output);
-		return responseHanded;
-	} else if (response.status == undefined) {
-		return response;
-	} else {
-		const responseHanded = await ServerResponseHandler(response, output);
-		return responseHanded;
-	}
+  const response = await ServerConnection(route, "GET", undefined, undefined);
+  if (response.status == 200) {
+    const responseHanded = await ServerResponseHandler(response, output);
+    return responseHanded;
+  } else if (response.status == undefined) {
+    return response;
+  } else {
+    const responseHanded = await ServerResponseHandler(response, output);
+    return responseHanded;
+  }
 };
 
 /**Descripcion: Realiza el fetch al servidor -
@@ -51,43 +63,43 @@ export const sendServerGet = async (route, output) => {
  *Retorna: Respuesta del servidor
  */
 const ServerConnection = async (url, method, body, authorization) => {
-	let headersList;
-	let httpOptions;
-	let response;
-	try {
-		if (authorization) {
-			headersList = {
-				Accept: "*/*",
-				"Access-Control-Allow-Origin": urlOrigin,
-				"Content-Type": "application/json",
-				Authorization: authorization,
-			};
-		} else {
-			headersList = {
-				Accept: "*/*",
-				"Access-Control-Allow-Origin": urlOrigin,
-				"Content-Type": "application/json",
-			};
-		}
+  let headersList;
+  let httpOptions;
+  let response;
+  try {
+    if (authorization) {
+      headersList = {
+        Accept: "*/*",
+        "Access-Control-Allow-Origin": urlOrigin,
+        "Content-Type": "application/json",
+        Authorization: authorization,
+      };
+    } else {
+      headersList = {
+        Accept: "*/*",
+        "Access-Control-Allow-Origin": urlOrigin,
+        "Content-Type": "application/json",
+      };
+    }
 
-		if (method === "GET") {
-			httpOptions = {
-				method: method,
-				headers: headersList,
-			};
-		} else {
-			httpOptions = {
-				method: method,
-				headers: headersList,
-				body: body,
-			};
-		}
+    if (method === "GET") {
+      httpOptions = {
+        method: method,
+        headers: headersList,
+      };
+    } else {
+      httpOptions = {
+        method: method,
+        headers: headersList,
+        body: body,
+      };
+    }
 
-		response = await fetch(urlServer + url, httpOptions);
-		return response;
-	} catch (error) {
-		return { tittle: "Error", message: error.message + " to URL:" + url };
-	}
+    response = await fetch(urlServer + url, httpOptions);
+    return response;
+  } catch (error) {
+    return { tittle: "Error", message: error.message + " to URL:" + url };
+  }
 };
 
 /**Descripcion: transforma la respuesta del servidor en un formato legible. -
@@ -97,6 +109,6 @@ const ServerConnection = async (url, method, body, authorization) => {
  *Retorna: Respuesta del servidor en formato legible
  */
 const ServerResponseHandler = async (response, type) => {
-	if (type === "text") return await response.text();
-	if (type === "json") return await response.json();
+  if (type === "text") return await response.text();
+  if (type === "json") return await response.json();
 };
