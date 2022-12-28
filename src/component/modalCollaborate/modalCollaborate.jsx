93ef9cb33dfaafda5339a9c3ba5b66844254f28d
Ivan H <ivan2herrerra@gmail.com>
2022-12-28 10:35:05 -0400

    notifications fixed

--- src/component/modalCollaborate/modalCollaborate.jsx
+++ src/component/modalCollaborate/modalCollaborate.jsx
@@ -17,9 +17,19 @@ const Transition = React.forwardRef(function Transition(props, ref) {
 	return <Slide direction="up" ref={ref} {...props} />;
 });
 
-export default function ModalCollaborate({referals,  userLogged, setUserLogged, open, setOpen, ticketId, addressReferer, packageId, value }) {
+export default function ModalCollaborate({
+	referals,
+	userLogged,
+	setUserLogged,
+	open,
+	setOpen,
+	ticketId,
+	addressReferer,
+	packageId,
+	value,
+}) {
 	const [openDisplay, setOpenDisplay] = useState(false);
-	const [openErrMeta, setOpenErrMeta] = useState(false);
+	const [openMessagesDisplay, setOpenMessagesDisplay] = useState(false);
 	const [message, setMessage] = useState({});
 	const [btnDis, setBtnDis] = useState(false);
 	const handleClickOpen = () => {
@@ -35,35 +45,26 @@ export default function ModalCollaborate({referals,  userLogged, setUserLogged, 
 		setOpen(false);
 	};
 
-
 	function btnDisabled(ref) {
 		if (ref == 4 && btnDis === false) {
-			console.log("true")
-			return	setBtnDis(true);
+			console.log("true");
+			return setBtnDis(true);
 		}
-		if (ref < 4 && btnDis === true){
-			console.log("falses")
-			return	setBtnDis(false);
+		if (ref < 4 && btnDis === true) {
+			console.log("falses");
+			return setBtnDis(false);
 		}
 	}
-	btnDisabled(referals)
+	btnDisabled(referals);
 	return (
-		<div >
-			<button
-	
-				disabled={btnDis}
-			 onClick={handleClickOpen} 
-			 className={style.btnPrincipal}
-			 >
+		<div>
+			<button disabled={btnDis} onClick={handleClickOpen} className={style.btnPrincipal}>
 				Collaborate
 			</button>
-
-
-
-<Dialog
+			<Dialog
 				sx={{
 					display: "center",
-					width: '100%'
+					width: "100%",
 				}}
 				open={open}
 				TransitionComponent={Transition}
@@ -71,94 +72,94 @@ export default function ModalCollaborate({referals,  userLogged, setUserLogged, 
 				onClose={handleClose}
 				aria-describedby="alert-dialog-slide-description"
 			>
-					<Button onClick={handleClose} 
-						sx={{
-							justifyContent:"end",
-							borderRadius: "8px",
-							color: "#51BADB",
-							fontSize: "20px",
-							
-						}}>
-					X
-					</Button>
+				<Button
+					onClick={handleClose}
+					sx={{
+						justifyContent: "end",
+						borderRadius: "8px",
+						color: "#51BADB",
+						fontSize: "20px",
+					}}
+				></Button>
 				<DialogContent>
-						<p className={style.titleModal}>
-							Select your payment method</p>
+					<p className={style.titleModal}>Select your payment method</p>
 				</DialogContent>
-				<DialogActions 	sx={{
-					
-							display:"flex",
-							flexDirection:"row",
-							justifyContent:"center"
-
-						}}>
-			
-				<button className={style.btnPago}
-					onClick={async () => {
-						await sendWriteTransactions(await sendServerGet("/addressCoin", "text"), await sendServerGet("/abiCoin", "json"), "approve", [
-							await sendServerGet("/addressContract", "text"),
-							value,
-						])
-							.then(async (response) => {
-								console.log(response);
-								await sendWriteTransactions(
-									await sendServerGet("/addressContract", "text"),
-									await sendServerGet("/abiContract", "json"),
-									"buyTicketSon",
-									[packageId, ticketId, addressReferer, true]
-								)
-									.then((response) => {
-										console.log(response);
-									})
-									.catch((error) => {
-										setOpenErrMeta(true);
-										setMessage({ tittle: "Metamask Error", message: error });
-									});
-							})
-							.catch((error) => {
-								setOpenErrMeta(true);
-								setMessage({ tittle: "Metamask Error", message: error });
-							});
+				<DialogActions
+					sx={{
+						display: "flex",
+						flexDirection: "row",
+						justifyContent: "center",
 					}}
 				>
-					{"BUSD (Binance USD)"}
-				</button>
-				<button
-				className={style.btnPago}
-					onClick={async () => {
-						await sendWriteTransactions(
-							await sendServerGet("/addressContract", "text"),
-							await sendServerGet("/abiContract", "json"),
-							"approve",
-							[await sendServerGet("/addressContract", "text"), value]
-						)
-							.then(async (response) => {
-								console.log(response);
-								await sendWriteTransactions(
-									await sendServerGet("/addressContract", "text"),
-									await sendServerGet("/abiContract", "json"),
-									"buyTicketSon",
-									[packageId, ticketId, addressReferer, false]
-								)
-									.then((response) => {
-										console.log(response);
-									})
-									.catch((error) => {
-										setOpenErrMeta(true);
-										setMessage({ tittle: "Metamask Error", message: error });
-									});
-							})
-							.catch((error) => {
-								setOpenErrMeta(true);
-								setMessage({ tittle: "Metamask Error", message: error });
-							});
-					}}
-				>
-					{"INFI (Infinitus Token)"}
-				</button>
-			
+					<button
+						className={style.btnPago}
+						onClick={async () => {
+							await sendWriteTransactions(await sendServerGet("/addressCoin", "text"), await sendServerGet("/abiCoin", "json"), "approve", [
+								await sendServerGet("/addressContract", "text"),
+								value,
+							])
+								.then(async (response) => {
+									console.log(response);
+									await sendWriteTransactions(
+										await sendServerGet("/addressContract", "text"),
+										await sendServerGet("/abiContract", "json"),
+										"buyTicketSon",
+										[packageId, ticketId, addressReferer, true]
+									)
+										.then((response) => {
+											console.log(response);
+											setOpenMessagesDisplay(true);
+											setMessage({ tittle: "Exito!", message: `Ticket comprado y registrado con el siguiente Hash:${response.Hash}` });
+										})
+										.catch((error) => {
+											setOpenMessagesDisplay(true);
+											setMessage({ tittle: "Metamask Error", message: error });
+										});
+								})
+								.catch((error) => {
+									setOpenMessagesDisplay(true);
+									setMessage({ tittle: "Metamask Error", message: error });
+								});
+						}}
+					>
+						{"BUSD (Binance USD)"}
+					</button>
+					<button
+						className={style.btnPago}
+						onClick={async () => {
+							await sendWriteTransactions(
+								await sendServerGet("/addressContract", "text"),
+								await sendServerGet("/abiContract", "json"),
+								"approve",
+								[await sendServerGet("/addressContract", "text"), value]
+							)
+								.then(async (response) => {
+									console.log(response);
+									await sendWriteTransactions(
+										await sendServerGet("/addressContract", "text"),
+										await sendServerGet("/abiContract", "json"),
+										"buyTicketSon",
+										[packageId, ticketId, addressReferer, false]
+									)
+										.then((response) => {
+											console.log(response);
+											setOpenMessagesDisplay(true);
+											setMessage({ tittle: "Exito!", message: `Ticket comprado y registrado con el siguiente Hash:${response.Hash}` });
+										})
+										.catch((error) => {
+											setOpenMessagesDisplay(true);
+											setMessage({ tittle: "Metamask Error", message: error });
+										});
+								})
+								.catch((error) => {
+									setOpenMessagesDisplay(true);
+									setMessage({ tittle: "Metamask Error", message: error });
+								});
+						}}
+					>
+						{"INFI (Infinitus Token)"}
+					</button>
 				</DialogActions>
-		
 			</Dialog>
 			<DisplayMessage
 				open={openDisplay}
@@ -171,7 +172,7 @@ export default function ModalCollaborate({referals,  userLogged, setUserLogged, 
 					clearUnusedProcess();
 				}}
 			/>
-			<DisplayMessage open={openErrMeta} messageData={message} setOpen={setOpenErrMeta} allowBackdropClick={true} />
+			<DisplayMessage open={openMessagesDisplay} messageData={message} setOpen={setOpenMessagesDisplay} allowBackdropClick={true} />
 		</div>
 	);
 }
