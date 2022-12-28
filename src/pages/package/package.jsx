fef77963206f546dd21925490a99ad6a5c8626c3
Roiner Bracamonte <rbracamonte.winhold@gmail.com>
2022-12-28 00:23:10 -0400

    Se realizado la validacion para cuando el estado del ticket este recolectado no aparezca en la lista de ticket

--- src/pages/package/package.jsx
+++ src/pages/package/package.jsx
@@ -1,57 +1,53 @@
- 
- import imgFilter from "../../assets/Light.png"
- import CardTikets from "../../component/cardTikets/cardTikets";
-import style from "./Package.module.css"
+import imgFilter from "../../assets/Light.png";
+import CardTikets from "../../component/cardTikets/cardTikets";
+import style from "./Package.module.css";
 import { sendServerPost } from "../../functions/serverInteractions";
 import { useState, useEffect } from "react";
-import tikets from "./infoTikets"
+import tikets from "./infoTikets";
 export default function Package() {
   const [myTickets, setmyTickets] = useState([]);
 
-  useEffect( () => {
-    const getTikets = async () =>{
-    	setmyTickets(
+  useEffect(() => {
+    const getTikets = async () => {
+      setmyTickets(
         await sendServerPost(
-          { address: localStorage.getItem('address') },
-          '/user/getmytickets',
-          'json',
-          localStorage.getItem('jwt')
+          { address: localStorage.getItem("address") },
+          "/user/getmytickets",
+          "json",
+          localStorage.getItem("jwt")
         )
       );
-      
-    }
-     getTikets()
-	}, []);
-
-
+    };
+    getTikets();
+    console.log(myTickets);
+  }, []);
+  console.log(myTickets);
   return (
     <div>
       <div className={style.contFilter}>
-        <img src={imgFilter} alt="" className={style.imgFilter}/>
+        <img src={imgFilter} alt="" className={style.imgFilter} />
       </div>
       <div className={style.cards}>
-
-       {
-          
-          Array.isArray(myTickets) ? myTickets.map((e) => {
-           
-           return (
-            <div key={e.ticketId}>
-                 <CardTikets 
-                     ticketId= {e.ticketId}
-                     referals= {e.referals}
-                     packageId={e.packageId}
-                     collected= {e.collected}
-                     imgRoute= {e.imgRoute}
-                />
-           </div>
-            )
-          })
-          
-          : (    <p> You have no tickets </p>)
-          
-         }
-    </div>
+        {Array.isArray(myTickets) ? (
+          myTickets
+            .filter((e) => e.collected !== true)
+            .map((e) => {
+              return (
+                <div key={e.ticketId}>
+                  <CardTikets
+                    ticketId={e.ticketId}
+                    referals={e.referals}
+                    packageId={e.packageId}
+                    collected={e.collected}
+                    imgRoute={e.imgRoute}
+                  />
+                </div>
+              );
+            })
+        ) : (
+          <p> You have no tickets </p>
+        )}
+      </div>
     </div>
   );
 }
